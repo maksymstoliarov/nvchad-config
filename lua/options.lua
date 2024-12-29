715ff15f6eda9b6d224ffc96b95765c821e3b52a
Maksym Stoliarov <windmill78max@gmail.com>
2024-12-29 15:37:01 +0100

    Treesitter languages added

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,13 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+
+o.colorcolumn = "80"
+
+
