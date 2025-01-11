fbf1ba00ca146c5d0db12adda52777f1420a89c1
Maksym Stoliarov <windmill78max@gmail.com>
2025-01-11 16:37:59 +0100

    Semicolon shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,3 +15,10 @@ map("i", "<C-l>", "<cmd> lua vim.fn.feedkeys(vim.fn['copilot#Accept'](), '') <cr
 -- go to implementation
 -- map("n", "gi", "<cmd> lua vim.lsp.buf.implementation() <cr>", { desc = "Go to Implementation" })
 -- map("n", "gI", "<cmd> lua vim.lsp.buf.type_definition() <cr>", { desc = "Go to Type Definition" })
+
+map(
+    "n",
+    "<Leader>;",
+    ':lua if string.sub(vim.api.nvim_get_current_line(), -1) ~= ";" then vim.cmd("normal! A;") end<CR>',
+    { noremap = true, silent = true }
+)
